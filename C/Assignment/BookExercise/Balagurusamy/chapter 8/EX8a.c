b7cc99f2209f2017516ecb4f5c8c28a3f17ad132
shiva kashyap <shivashahi32116@gmail.com>
2020-11-24 12:18:08 +0530

    20201124

---
+++ C/Assignment/BookExercise/Balagurusamy/chapter 8/EX8a.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+	intk,i,l;
+	char dinar[100];
+	printf(Enter a STRING:\n");
+	gets(dinar);
+	i=strlen(dinar);
+	printf("the ASCII values of  the strinng are:\n");
+	for(i=0;i<|;i++)
+		{
+		  k=dinar[i];
+		  printf("%d",k);
+		 }
+}
