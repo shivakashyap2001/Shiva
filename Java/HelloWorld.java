7fa1bbc305af274948a6f468dfacb2850f22d634
shiva kashyap <shivashahi32116@gmail.com>
2021-05-19 14:21:05 +0530

    Modified HelloWorld.java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,19 +1,8 @@
-import java.util.Scanner;
 
-public class HelloWorld 
+public class HelloWorld
 {
 	public static void main(String[] args)
 	{
-		// creates a reader instance which takes
-		// input from standard input _ keyboard
-		Scanner reader = new Scanner (System.in);
-		System.out.print("Enter a number :");
-	
-		// nextInt() reads the next integer  from the keyboard
-		int number = reader.nextInt();
-	
-		//println() prints the following line to the output screen
-		System.out.println("You entered:"+number);
+		System.out.println("Hello World!");
 	}
 }
-	
